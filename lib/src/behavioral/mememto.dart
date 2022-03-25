ddd3b2bcc0d1de0498c6b8901fb038aee2440fe8
sawthiha <sawthiha@mtu.edu.mm>
2022-03-25 14:02:38 +0900

    Mememto module documented!

--- lib/src/behavioral/mememto.dart
+++ lib/src/behavioral/mememto.dart
@@ -1,10 +1,12 @@
 part of behavioral;
 
-abstract class Mememto  {
-  
-}
+/// Empty Mememto Interface
+abstract class Mememto  { }
 
+/// Mememto Originator Interface
 abstract class MememtoOriginator<T extends Mememto>  {
+  /// Export current mememto
   Mememto save();
+  /// Import state by mememto
   void load(T mememto);
 }
