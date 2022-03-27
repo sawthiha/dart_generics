79b46089eed7fabce2f1ee26552d5acd2eb89891
sawthiha <sawthiha@mtu.edu.mm>
2022-03-27 18:03:22 +0900

    Specification module documented!

--- lib/src/behavioral/specification.dart
+++ lib/src/behavioral/specification.dart
@@ -1,26 +1,39 @@
+/// Specfication Module
 part of behavioral;
 
+/// Specification Interface
 abstract class Specification<T>  {
+
   const Specification();
   
+  /// Check if object is satisfied
   bool isSatisfiedBy(T obj);
 
+  /// Predicate Specification factory
   factory Specification.base(
     bool Function(T) specification
   ) = SpecificationBase;
 
+  /// Type Specification pattern
   static Specification type<T>() => TypeSpecification<T>();
 }
 
+/// Specification Composition Operations
+/// 
+/// Logical AND, OR, NOT
 extension CompositedSpecification<T> on Specification<T>  {
   
+  /// Chain two specifications in AND logic
   Specification<T> and(Specification<T> other) => AndSpecification([this, other]);
 
+  /// Chain two specifications in OR logic
   Specification or(Specification other) => OrSpecification([this, other]);
 
+  /// Negate the specification
   Specification not() => NegationSpecification(this);
 }
 
+/// Type Specification
 class TypeSpecification<T> implements Specification  {
   
   @override
@@ -28,7 +41,9 @@ class TypeSpecification<T> implements Specification  {
 
 }
 
+/// Concrete Predicated Specification
 class SpecificationBase<T> extends Specification<T>  {
+  /// Predicate
   final bool Function(T) specification;
 
   SpecificationBase(this.specification);
@@ -38,14 +53,16 @@ class SpecificationBase<T> extends Specification<T>  {
 
 }
 
+/// Composited Specification Interface
 abstract class CompositeSpecification<T> extends Specification<T> {
+  /// Specifications to evaluate
   final List<Specification<T>> expressions;
   
   const CompositeSpecification(this.expressions);
   
 }
 
-
+/// AND logic [CompositeSpecification]
 class AndSpecification<T> extends CompositeSpecification<T>  {
   const AndSpecification(List<Specification<T>> expressions): super(expressions);
   
@@ -56,6 +73,7 @@ class AndSpecification<T> extends CompositeSpecification<T>  {
   
 }
 
+/// OR logic [CompositeSpecification]
 class OrSpecification<T> extends CompositeSpecification<T>  {
   const OrSpecification(List<Specification<T>> expressions): super(expressions);
   
@@ -65,7 +83,9 @@ class OrSpecification<T> extends CompositeSpecification<T>  {
   );
 }
 
+/// Negation [Specification]
 class NegationSpecification<T> extends Specification<T>  {
+  /// Specification to be negated
   final Specification negatee;
   
   const NegationSpecification(this.negatee);
